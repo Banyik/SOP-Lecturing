f3c522a33617ec238052f59630226212924c787c
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2023-09-13 14:37:17 +0200

    Initial Commit, first course

---
+++ SOP_DrawRandomLines/SOP_DrawRandomLines/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace SOP_DrawRandomLines
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.DrawLines = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.Color.White;
+            this.canvas.Location = new System.Drawing.Point(12, 12);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(678, 426);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            // 
+            // DrawLines
+            // 
+            this.DrawLines.Location = new System.Drawing.Point(696, 12);
+            this.DrawLines.Name = "DrawLines";
+            this.DrawLines.Size = new System.Drawing.Size(92, 23);
+            this.DrawLines.TabIndex = 1;
+            this.DrawLines.Text = "Draw Lines";
+            this.DrawLines.UseVisualStyleBackColor = true;
+            this.DrawLines.Click += new System.EventHandler(this.DrawLines_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DrawLines);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button DrawLines;
+    }
+}
+
---
+++ SOP_DrawRandomLines/SOP_DrawRandomLines/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace SOP_DrawRandomLines
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            g = canvas.CreateGraphics();
+        }
+
+        void DrawRandomLines()
+        {
+            int x1 = rnd.Next(canvas.Width);
+            int y1 = rnd.Next(canvas.Height);
+            int x2 = rnd.Next(canvas.Width);
+            int y2 = rnd.Next(canvas.Height);
+            lock (g)
+            {
+                g.DrawLine(Pens.Black, x1, y1, x2, y2);
+            }
+            Thread.Sleep(1000);
+            lock (g)
+            {
+                g.DrawLine(Pens.White, x1, y1, x2, y2);
+            }
+        }
+
+        void DrawHorizontalLines(int x, int y)
+        {
+            lock (g)
+            {
+                g.DrawLine(Pens.Black, x, y, canvas.Width, y);
+            }
+        }
+
+        private void DrawLines_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                //Thread t = new Thread(() => DrawHorizontalLines(0, i));
+                Thread t = new Thread(DrawRandomLines);
+                t.Start();
+            }
+
+        }
+    }
+}
