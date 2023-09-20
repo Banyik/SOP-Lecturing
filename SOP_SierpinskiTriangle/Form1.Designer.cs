9b3481fc798865e74e43595c66fc26437e8dbe35
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2023-09-20 14:46:33 +0200

    Added second lecture; sierpinski triangle

---
+++ SOP_SierpinskiTriangle/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace SOP_SierpinskiTriangle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.drawTriangles = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.Location = new System.Drawing.Point(12, 12);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(682, 426);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            // 
+            // drawTriangles
+            // 
+            this.drawTriangles.Location = new System.Drawing.Point(700, 12);
+            this.drawTriangles.Name = "drawTriangles";
+            this.drawTriangles.Size = new System.Drawing.Size(88, 23);
+            this.drawTriangles.TabIndex = 1;
+            this.drawTriangles.Text = "Draw Triangles";
+            this.drawTriangles.UseVisualStyleBackColor = true;
+            this.drawTriangles.Click += new System.EventHandler(this.drawTriangles_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.drawTriangles);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button drawTriangles;
+    }
+}
+
---
+++ SOP_SierpinskiTriangle/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace SOP_SierpinskiTriangle
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        int depth = 8;
+        const int MAX_THREAD_COUNT = 3;
+        int threadCount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            g = canvas.CreateGraphics();
+        }
+
+        void DrawBaseTriangle()
+        {
+            Point a = new Point(canvas.Width / 2, 10);
+            Point b = new Point(10, canvas.Height - 10);
+            Point c = new Point(canvas.Width - 10, canvas.Height - 10);
+            g.DrawPolygon(Pens.Black, new Point[] { a, b, c });
+            DrawTriangles(a, b, c, depth);
+        }
+
+        void DrawTriangles(Point a, Point b, Point c, int depth)
+        {
+            if(depth < 0)
+            {
+                return;
+            }
+            lock (g)
+            {
+                g.DrawLine(Pens.Black, b, c);
+            }
+            Point mAB = GetMiddlePoint(a, b);
+            Point mAC = GetMiddlePoint(a, c);
+            Point mBC = GetMiddlePoint(b, c);
+            int currentDepth = depth - 1;
+            Thread t1 = new Thread(() => DrawTriangles(a, mAB, mAC, currentDepth));
+            if(threadCount < MAX_THREAD_COUNT)
+            {
+                t1.Start();
+                threadCount++;
+            }
+            else
+            {
+                DrawTriangles(a, mAB, mAC, currentDepth);
+            }
+            Thread t2 = new Thread(() => DrawTriangles(b, mAB, mBC, currentDepth));
+            if (threadCount < MAX_THREAD_COUNT)
+            {
+                t2.Start();
+                threadCount++;
+            }
+            else
+            {
+                DrawTriangles(b, mAB, mBC, currentDepth);
+            }
+            Thread t3 = new Thread(() => DrawTriangles(c, mAC, mBC, currentDepth));
+            if (threadCount < MAX_THREAD_COUNT)
+            {
+                t3.Start();
+                threadCount++;
+            }
+            else
+            {
+                DrawTriangles(c, mAC, mBC, currentDepth);
+            }
+            if(t1.ThreadState != ThreadState.Unstarted)
+            {
+                t1.Join();
+                threadCount--;
+            }
+            if (t2.ThreadState != ThreadState.Unstarted)
+            {
+                t2.Join();
+                threadCount--;
+            }
+            if (t3.ThreadState != ThreadState.Unstarted)
+            {
+                t3.Join();
+                threadCount--;
+            }
+        }
+
+        Point GetMiddlePoint(Point p0, Point p1)
+        {
+            int x = (p0.X + p1.X) / 2;
+            int y = (p0.Y + p1.Y) / 2;
+            return new Point(x, y);
+        }
+
+        private void drawTriangles_Click(object sender, EventArgs e)
+        {
+            DrawBaseTriangle();
+        }
+    }
+}
