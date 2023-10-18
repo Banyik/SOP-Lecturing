6911c0fb928bd46f871fe9be0c142d9c628c3ab1
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2023-10-18 14:42:16 +0200

    Added server client

---
+++ SOP_ServerClient/Client/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Configuration;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    internal class Program
+    {
+        static StreamReader reader;
+        static StreamWriter writer;
+        static TcpClient client;
+        static Thread rInfoThread = new Thread(ReadInfo);
+        static void Main(string[] args)
+        {
+            string ip = ConfigurationManager.AppSettings["ip"].ToString();
+            int port = int.Parse(ConfigurationManager.AppSettings["port"].ToString());
+            try
+            {
+                client = new TcpClient(ip, port);
+                writer = new StreamWriter(client.GetStream());
+                reader = new StreamReader(client.GetStream());
+                rInfoThread.Start();
+                Console.WriteLine($"Connected to {ip}:{port}");
+                Send();
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Something went wrong...");
+            }
+        }
+
+        static void ReadInfo()
+        {
+            try
+            {
+                while (!reader.EndOfStream)
+                {
+                    Console.WriteLine(reader.ReadLine());
+                }
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                Console.WriteLine("Timed out...");
+            }
+        }
+        static void Send()
+        {
+            try
+            {
+                while (true)
+                {
+                    Write(Console.ReadLine());
+                }
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+        static void Write(string msg)
+        {
+            writer.WriteLine(msg);
+            writer.Flush();
+        }
+
+        static void Close()
+        {
+            reader.Close();
+            writer.Close();
+            client.Close();
+        }
+    }
+}
---
+++ SOP_ServerClient/Server/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Configuration;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    internal class Program
+    {
+        static bool listen = true;
+        static TcpListener server;
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse(ConfigurationManager.AppSettings["ip"].ToString());
+            int port = int.Parse(ConfigurationManager.AppSettings["port"].ToString());
+
+            server = new TcpListener(ip, port);
+            server.Start();
+
+            new Thread(WaitForClients).Start();
+
+            Console.WriteLine("Press enter to shut down the server...");
+            Console.ReadLine();
+
+            listen = false;
+        }
+
+        static void WaitForClients()
+        {
+            while (listen)
+            {
+                if (server.Pending())
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    new Thread(() => ManageClient(client)).Start();
+                }
+            }
+            Environment.Exit(Environment.ExitCode);
+        }
+
+        static void ManageClient(TcpClient client)
+        {
+            StreamReader reader = new StreamReader(client.GetStream());
+            StreamWriter writer = new StreamWriter(client.GetStream());
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine($"Received message: {reader.ReadLine()}");
+                    writer.WriteLine("Message received!");
+                    writer.Flush();
+                }
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                reader.Close();
+                writer.Close();
+                client.Close();
+                Console.WriteLine("An error has occured while listening to the client...");
+            }
+        }
+    }
+}
