9ee7e60bb7acc5f1d25e56d9de63310e406cd713
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2023-11-22 15:10:45 +0100

    Added client files p2

---
+++ SOP_RestAPI/sop/Client/Main.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestSharp;
+
+namespace SOP_Client
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            InitializeUsersDataGridView();
+            RefreshUserData();
+        }
+
+        RestClient restClient = new RestClient("http://localhost/sop/server/users.php");
+        void InitializeUsersDataGridView()
+        {
+            usersData.Columns.Add("id", "ID");
+            usersData.Columns.Add("username", "Username");
+            usersData.Columns.Add("password", "Password");
+        }
+        public void RefreshUserData()
+        {
+            RestRequest request = new RestRequest();
+            try
+            {
+                RestResponse response = restClient.Get(request);
+                if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                {
+                    MessageBox.Show(response.StatusDescription);
+                }
+                else
+                {
+                    Response res = restClient.Deserialize<Response>(response).Data;
+                    usersData.Rows.Clear();
+                    foreach (var user in res.Users)
+                    {
+                        usersData.Rows.Add(user.ID, user.username, user.password);
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void getButton_Click(object sender, EventArgs e)
+        {
+            RefreshUserData();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            ManageButtons(false);
+            new UserHandler(restClient, this, RequestType.POST).Show();
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            ManageButtons(false);
+            new UserHandler(restClient, this, RequestType.PUT).Show();
+        }
+
+        public void ManageButtons(bool enable)
+        {
+            getButton.Enabled = enable;
+            addButton.Enabled = enable;
+            updateButton.Enabled = enable;
+        }
+    }
+}
